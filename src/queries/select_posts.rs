632a7784ba0cebf453018b2821cdb5aa910c6d78
Timo Caktu <timocaktu@gmail.com>
2023-07-27 02:30:03 +0200

    new endpoints on the way

--- src/queries/select_posts.rs
+++ src/queries/select_posts.rs
@@ -1,18 +1,28 @@
+use self::schema::posts::dsl::*;
+use crate::{models::post::Post, *};
 use diesel::prelude::*;
-use crate::{*, models::post::Post};
-
 /*
 select all users for validation if the username is unique!
 */
 pub fn get_posts() -> Vec<Post> {
-    use self::schema::posts::dsl::*;
-
     let conn = &mut establish_connection();
 
     let results = posts
         .select(Post::as_select())
         .load(conn)
         .expect("Error loading posts");
+
+    results
+}
+
+pub fn get_posts_by_answer_id(filter_answer_id: i32) -> Vec<Post> {
+    let conn = &mut establish_connection();
+
+    let results = posts
+        .filter(id.eq(filter_answer_id))
+        .select(Post::as_select())
+        .load(conn)
+        .expect("error loading posts!");
 
     results
 }
